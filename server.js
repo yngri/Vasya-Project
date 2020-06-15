3b8a3704bbee387c67d66c193da0f0b384191d50
= <=>
2020-06-14 23:48:01 -0400

    Added simple client form that right now just logs the received request body to the console and redirects back to root of application

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const bodyParser = require("body-parser");
 var app = express();
 const MongoClient = require("mongodb").MongoClient;
 const connectionString =
@@ -15,22 +16,36 @@ MongoClient.connect(
     // ... do something here
     if (err) return console.error(err);
     const db = client.db("test-db");
-    const testConnection = db.collection("test");
-
-    testConnection
-      .insertOne({
-        name: "name",
-        value: "one",
-      })
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => console.error(error));
+    testConnection = db.collection("test");
+    //console.log(testConnection);
   }
 );
 
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  //res.send("Hello World");
+  res.sendFile(__dirname + "/test.html");
+});
+
+app.post("/quotes", function (req, res) {
+  var requestObjectReceivedByServer = req.body;
+  console.log("received a quote object that looks as follows:\n");
+  console.log(req.body);
+
+  console.log("testCOnnection: " + testConnection);
+  //   testConnection
+  //     .insertOne({
+  //       name: requestObjectReceivedByServer.name,
+  //       value: requestObjectReceivedByServer.quote,
+  //     })
+  //     .then((result) => {
+  //       console.log(result);
+  //     })
+  //     .catch((error) => console.error(error));
+
+  res.redirect("/");
 });
 
 var server = app.listen(3000, function () {
