b9606bdf564cec019aee1ec6e55e24caa846f356
Alex <thisisalexprodanov@gmail.com>
2020-06-18 17:22:09 -0400

    updated routing once again

--- routes/quotes.js
+++ routes/quotes.js
@@ -5,6 +5,17 @@ router.post("/", function (req, res) {
     var requestObjectReceivedByServer = req.body;
     console.log("received a quote object that looks as follows:\n");
     console.log(req.body);
+
+    console.log("testCOnnection: " + testConnection);
+    //   testConnection
+    //     .insertOne({
+    //       name: requestObjectReceivedByServer.name,
+    //       value: requestObjectReceivedByServer.quote,
+    //     })
+    //     .then((result) => {
+    //       console.log(result);
+    //     })
+    //     .catch((error) => console.error(error));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -15,78 +15,22 @@ app.use('/', home);
 
 
 const connectionString =
-  "mongodb+srv://admin:admin@cluster0-mtnrw.mongodb.net/<dbname>?retryWrites=true&w=majority";
+    "mongodb+srv://admin:admin@cluster0-mtnrw.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
-//  Make sure you place body-parser before your CRUD handlers!
-<<<<<<< HEAD
-
-
-
-=======
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-
-app.get("/", function (req, res) {
-  //res.send("Hello World");
-  res.sendFile(__dirname + "/test.html");
-});
->>>>>>> 21f3a5aa8d4b7e32c27935d3b21cd8f910b37b4c
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 );
 
 MongoClient.connect(
-  connectionString,
-  {
-    useUnifiedTopology: true,
-  },
-  (err, client) => {
-    // ... do something here
-    if (err) return console.error(err);
-    const db = client.db("test-db");
-    testConnection = db.collection("test");
-    //console.log(testConnection);
-  }
+    connectionString, {
+        useUnifiedTopology: true,
+    },
+    (err, client) => {
+        // ... do something here
+        if (err) return console.error(err);
+        const db = client.db("test-db");
+        testConnection = db.collection("test");
+        //console.log(testConnection);
+    }
 );
-
-<<<<<<< HEAD
-console.log("testCOnnection: " + testConnection);
-//   testConnection
-//     .insertOne({
-//       name: requestObjectReceivedByServer.name,
-//       value: requestObjectReceivedByServer.quote,
-//     })
-//     .then((result) => {
-//       console.log(result);
-//     })
-//     .catch((error) => console.error(error));
-//});
-=======
-app.post("/quotes", function (req, res) {
-  var requestObjectReceivedByServer = req.body;
-  console.log("received a quote object that looks as follows:\n");
-  console.log(req.body);
-
-  console.log("testCOnnection: " + testConnection);
-  //   testConnection
-  //     .insertOne({
-  //       name: requestObjectReceivedByServer.name,
-  //       value: requestObjectReceivedByServer.quote,
-  //     })
-  //     .then((result) => {
-  //       console.log(result);
-  //     })
-  //     .catch((error) => console.error(error));
-});
->>>>>>> 21f3a5aa8d4b7e32c27935d3b21cd8f910b37b4c
-
-<<<<<<< HEAD
-//123
-//Comment from max
-=======
-//comment from Alex
->>>>>>> 07bde7ff8aeb8b13ef12e2c468a6f03c9be47c86
