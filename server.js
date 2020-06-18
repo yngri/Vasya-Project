372ad9e2fb9cb3ff9f406b32ed014a4834af73ae
Alex <thisisalexprodanov@gmail.com>
2020-06-18 17:56:26 -0400

    added comments for app.use

--- server.js
+++ server.js
@@ -6,26 +6,25 @@ const express = require("express");
 const app = express();
 const port = 3003;
 
-app.use("/quotes", quotes);
-app.use("/", home);
+app.use("/quotes", quotes); //uses imported router from quotes const on /quotes endpoint
+app.use("/", home); //uses imported router from homes const on / endpoint
 
 const connectionString =
-  "mongodb+srv://admin:admin@cluster0-mtnrw.mongodb.net/<dbname>?retryWrites=true&w=majority";
+    "mongodb+srv://admin:admin@cluster0-mtnrw.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 );
 
 MongoClient.connect(
-  connectionString,
-  {
-    useUnifiedTopology: true,
-  },
-  (err, client) => {
-    // ... do something here
-    if (err) return console.error(err);
-    const db = client.db("test-db");
-    testConnection = db.collection("test");
-    //console.log(testConnection);
-  }
+    connectionString, {
+        useUnifiedTopology: true,
+    },
+    (err, client) => {
+        // ... do something here
+        if (err) return console.error(err);
+        const db = client.db("test-db");
+        testConnection = db.collection("test");
+        //console.log(testConnection);
+    }
 );
